f33024ae47c9af871d6264eae2331f26617e9b26
acupoflie <127441535+acupoflie@users.noreply.github.com>
2023-11-12 20:16:20 +0400

    Add files via upload

--- jsTutorials/nodejsTutorials/app.js
+++ jsTutorials/nodejsTutorials/app.js
@@ -1,21 +1,63 @@
 
 
-
-
-
-
 const readline =  require('readline')
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const fs = require('fs')
+const http = require('http')
 
-rl.question("Please enter your name: ", (name) => {
-    console.log("You entered " + name);
-    rl.close();
+//TODO - WRITE AND READ CONSOLE
+//********************************************* */
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question("Please enter your name: ", (name) => {
+//     console.log("You entered " + name);
+//     rl.close();
+// })
+
+// rl.on('close', () => {
+//     console.log("Interface closed");
+//     process.exit(0)
+// })
+//********************************************* */
+
+
+//TODO - READING AND WRITING FILE SYNC
+// let datafs = fs.readFileSync('./files/example.txt', 'utf-8')
+// console.log(datafs)
+
+// let context = `Data text from previous txt: ${datafs} \nDate created ${new Date()}`;
+// fs.writeFileSync('./files/example.txt', context)
+// ********************************************* */
+
+
+
+//TODO - READING AND WRITING FILE ASYNC
+// fs.readFile('./files/start.txt', 'utf-8', (error1, data1) => {
+//     console.log(data1)
+//     fs.readFile(`./files/${data1}.txt`, 'utf-8', (error2, data2) => {
+//         console.log(data2)
+//         fs.readFile(`./files/append.txt`, 'utf-8', (error3, data3) => {
+//             console.log(data3)
+//             fs.writeFile('./files/output.txt', `${data2}\n\n${data3}\n\nData created ${new Date()}`, () => {
+//                 console.log("FILE WRITTEN SUCCESFULLY")
+//             })
+//         })
+//     })
+// })
+// console.log("fak this file")
+// ********************************************* */
+
+
+//TODO - SERVER
+
+const server = http.createServer((request, response) => {
+    response.end("hello from the server")
+    console.log("New Request")
+    // console.log(response)
 })
 
-rl.on('close', () => {
-    console.log("Interface closed");
-    process.exit(0)
+server.listen(8000, '127.0.0.1', () => {
+    console.log("server started")
 })
