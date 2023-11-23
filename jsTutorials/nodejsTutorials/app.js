e11d8a18a338e19010cf6f4ae367e7e6c65013c9
acupoflie <ishyeen@gmail.com>
2023-11-24 03:06:19 +0400

    update

--- jsTutorials/nodejsTutorials/app.js
+++ jsTutorials/nodejsTutorials/app.js
@@ -1,10 +1,12 @@
 
 
-const readline =  require('readline')
+const readline = require('readline')
 const fs = require('fs')
 const http = require('http')
 const path = require('path')
 const { json } = require('stream/consumers')
+const url = require('url')
+const replaceHtml = require('./modules/replaceHtml')
 
 //TODO - WRITE AND READ CONSOLE
 //********************************************* */
@@ -57,55 +59,91 @@ const { json } = require('stream/consumers')
 const html = fs.readFileSync('./template/index.html', 'utf-8')
 let products = JSON.parse(fs.readFileSync('./data/products.json', 'utf-8'))
 let productListHtml = fs.readFileSync('./template/product-list.html', 'utf-8')
+let productDetailHtml = fs.readFileSync('./template/product-details.html', 'utf-8')
 
-let productsHtmlArray = products.map((prod) => {
-    let output = productListHtml.replace('{{%IMAGE%}}', prod.productImage)
-    output = output.replace('{{%NAME%}}', prod.name)
-    output = output.replace('{{%MODELNAME%}}', prod.modeName)
-    output = output.replace('{{%MODELNO%}}', prod.modelNumber)
-    output = output.replace('{{%SIZE%}}', prod.size)
-    output = output.replace('{{%CAMERA%}}', prod.camera)
-    output = output.replace('{{%PRICE%}}', prod.price)
-    output = output.replace('{{%COLOR%}}', prod.color)
+// let productsHtmlArray = products.map((prod) => {
+//     let output = productListHtml.replace('{{%IMAGE%}}', prod.productImage)
+//     output = output.replace('{{%NAME%}}', prod.name)
+//     output = output.replace('{{%MODELNAME%}}', prod.modeName)
+//     output = output.replace('{{%MODELNO%}}', prod.modelNumber)
+//     output = output.replace('{{%SIZE%}}', prod.size)
+//     output = output.replace('{{%CAMERA%}}', prod.camera)
+//     output = output.replace('{{%PRICE%}}', prod.price)
+//     output = output.replace('{{%COLOR%}}', prod.color)
+//     output = output.replace('{{%ID%}}', prod.id)
 
-    return output
-})
+//     return output
+// })
 
-const server = http.createServer((request, response) => {
-    let path = request.url;
+// function replaceHtml(template, product){
+//     let output = template.replace('{{%IMAGE%}}', product.productImage)
+//     output = output.replace('{{%NAME%}}', product.name)
+//     output = output.replace('{{%MODELNAME%}}', product.modeName)
+//     output = output.replace('{{%MODELNO%}}', product.modelNumber)
+//     output = output.replace('{{%SIZE%}}', product.size)
+//     output = output.replace('{{%CAMERA%}}', product.camera)
+//     output = output.replace('{{%PRICE%}}', product.price)
+//     output = output.replace('{{%COLOR%}}', product.color)
+//     output = output.replace('{{%ID%}}', product.id)
+//     output = output.replace('{{%ROM%}}', product.ROM)
+//     output = output.replace('{{%DESC%}}', product.Description)
+
+//     return output
+// }
+
+// const server = http.createServer((request, response) => {
+//      CONTENT
+// });
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
     
+    let {query, pathname: path} = url.parse(request.url, true)
+
+    // let path = request.url;
+
     // HOME PAGE
-    if(path === "/" || path.toLocaleLowerCase() === "/home") {
+    if (path === "/" || path.toLocaleLowerCase() === "/home") {
         response.writeHead(200, {
-            'Content-type' : 'text/html',
-            'my-header' : 'hello mother fucker'
+            'Content-type': 'text/html',
+            'my-header': 'hello mother fucker'
         })
         response.end(html.replace('{{%CONTENT%}}', "Home Page"))
-    // ABOUT PAGE
+        // ABOUT PAGE
     } else if (path.toLocaleLowerCase() === "/about") {
         response.writeHead(200, {
-            'Content-type' : 'text/html',
-            'my-header' : 'hello mother fucker'
+            'Content-type': 'text/html',
+            'my-header': 'hello mother fucker'
         })
         response.end(html.replace('{{%CONTENT%}}', "About Page"))
-    // CONTACT PAGE
+        // CONTACT PAGE
     } else if (path.toLocaleLowerCase() === "/contact") {
         response.writeHead(200, {
-            'Content-type' : 'text/html',
-            'my-header' : 'hello mother fucker'
+            'Content-type': 'text/html',
+            'my-header': 'hello mother fucker'
         })
         response.end(html.replace('{{%CONTENT%}}', "Contact Page"))
-    // PRODUCTS PAGE
+        // PRODUCTS PAGE
     } else if (path.toLocaleLowerCase() === "/products") {
-        let productResponse = html.replace('{{%CONTENT%}}', productsHtmlArray.join(','))
-        response.writeHead(200, {'Content-type' : 'text/html'})
-        response.end(productResponse)
-        // console.log(productsHtmlArray.join(','))
-    // ERROR PAGE
+        if(!query.id) {
+            let productsHtmlArray = products.map((prod) => {
+                return replaceHtml(productListHtml, prod)
+            })
+            let productResponse = html.replace('{{%CONTENT%}}', productsHtmlArray.join(','))
+            response.writeHead(200, {'Content-type' : 'text/html'})
+            response.end(productResponse)
+        } else {
+            let prod = products[query.id]
+            let productDetailResponseHtml = replaceHtml(productDetailHtml, prod)
+            response.end(html.replace('{{%CONTENT%}}', productDetailResponseHtml))
+        }
+        // console.log(productsHtmlArray.join(''))
+        // ERROR PAGE
     } else {
         response.writeHead(404, {
-            'Content-type' : 'text/html',
-            'my-header' : 'hello mother fucker'
+            'Content-type': 'text/html',
+            'my-header': 'hello mother fucker'
         })
         response.end(html.replace('{{%CONTENT%}}', "Error 404: Page not found"))
     }
