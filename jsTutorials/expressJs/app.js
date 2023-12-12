3e4fb1eb0ab311c660fc1c4756aca5bc4ce1f03d
Sir <ishyeen@gmail.com>
2023-12-12 15:02:56 +0400

    initialize npm to expressjs

---
+++ jsTutorials/expressJs/app.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express');
+
+let app = express();
+
+
+app.get('/', (req, res) => {
+    
+})
+
+
+const port = 5000;
+app.listen(port, () => {
+    console.log("server has started")
+})
