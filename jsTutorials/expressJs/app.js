05c10ed544c64380adeb2fd43a12fd7c818a72db
Sir <ishyeen@gmail.com>
2023-12-13 19:20:12 +0400

    update get router for rest api

--- jsTutorials/expressJs/app.js
+++ jsTutorials/expressJs/app.js
@@ -1,16 +1,26 @@
 
 
 const express = require('express');
+const fs = require('fs')
 
 let app = express();
+let movies = JSON.parse(fs.readFileSync('./data/movies.json', 'utf-8'));
 
-
-app.get('/', (req, res) => {
-    
+app.get('/api/v1/movies', (req, res) => {
+    res.status(200).json({
+        // Enveloping JSON data, JSEND format
+        status : "secces",
+        count : movies.length,
+        data : {
+            movies : movies
+        }
+    })
 })
 
 
+
+// CREATE SERVER
 const port = 5000;
 app.listen(port, () => {
-    console.log("server has started")
+    console.log("server has started");
 })
