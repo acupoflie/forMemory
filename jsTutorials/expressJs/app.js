e5e81df5a1790f11fc76b38c240d66899c2e847e
Sir <ishyeen@gmail.com>
2023-12-13 21:09:37 +0400

    post request with jsen format

--- jsTutorials/expressJs/app.js
+++ jsTutorials/expressJs/app.js
@@ -6,6 +6,9 @@ const fs = require('fs')
 let app = express();
 let movies = JSON.parse(fs.readFileSync('./data/movies.json', 'utf-8'));
 
+app.use(express.json())
+
+// GET request
 app.get('/api/v1/movies', (req, res) => {
     res.status(200).json({
         // Enveloping JSON data, JSEND format
@@ -17,7 +20,25 @@ app.get('/api/v1/movies', (req, res) => {
     })
 })
 
+// POST request
+app.post('/api/v1/movies', (req, res) => {
+    // console.log(req.body)
+    const newID = movies[movies.length - 1].id + 1;
 
+    const newMovie = Object.assign({id: newID}, req.body);
+    movies.push(newMovie)
+
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        res.status(201).json({
+            status : "success",
+            data : {
+                movie: newMovie
+            }
+        })
+    })
+
+    // res.send('Created')
+})
 
 // CREATE SERVER
 const port = 5000;
