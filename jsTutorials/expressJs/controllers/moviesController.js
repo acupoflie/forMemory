8f04f9a797e859831c2c89376bdbe952876c3a48
Sir <ishyeen@gmail.com>
2023-12-24 04:59:16 +0400

    pagination for ui

--- jsTutorials/expressJs/controllers/moviesController.js
+++ jsTutorials/expressJs/controllers/moviesController.js
@@ -38,6 +38,8 @@ exports.getAllMovies = async (req, res) => {
         //! these lines is important for Mongoose 7.0 or later
         delete queryObj.sort
         delete queryObj.fields
+        delete queryObj.page
+        delete queryObj.limit
         let query = Movie.find(queryObj)
 
         // Sorting logic
@@ -54,6 +56,19 @@ exports.getAllMovies = async (req, res) => {
             query = query.select(fields)
         } else {
             query = query.select('-__v')
+        }
+
+        // Pagination
+        const page = +req.query.page || 1;
+        const limit = +req.query.limit || 10;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page) {
+            const moviesCount = await Movie.countDocuments();
+            if(skip >= moviesCount) {
+                throw new Error('This page is not found!')
+            }
         }
 
         const movies = await query;
