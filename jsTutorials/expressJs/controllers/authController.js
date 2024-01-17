d3ca4e31512dfed41fbefdd5feb4e6ed4ea98cfa
acupoflie <ishyeen@gmail.com>
2024-01-17 19:10:38 +0400

    storing jwt in the httpOnly cookie

--- jsTutorials/expressJs/controllers/authController.js
+++ jsTutorials/expressJs/controllers/authController.js
@@ -17,6 +17,19 @@ const signToken = id => {
 const createSendResponse = (user, statusCode, res) => {
     const token = signToken(user._id);
 
+    const options = {
+        expiresIn: process.env.LOGIN_EXPIRES,
+        httpOnly: true
+    }
+
+    if(process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
+    res.cookie('jwt', token, options);
+
+    user.password = undefined;
+
     res.status(statusCode).json({
         status: "success",
         token,
