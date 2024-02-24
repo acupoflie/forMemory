74168e490dcd32f0bebf5195aea5a2048cc0f8af
acupoflie <ishyeen@gmail.com>
2024-02-24 23:22:40 +0400

    improving ts knowledge

---
+++ tsProjects/src/helpers/index.ts
@@ -0,0 +1,9 @@
+
+import crypto from 'crypto';
+
+const SECRET = 'ljasdf8-lkasdf0-23nasdf'
+
+export const random = () => crypto.randomBytes(128).toString('base64');
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('base64');
+}
---
+++ tsProjects/src/index.ts
@@ -0,0 +1,34 @@
+
+
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import router from './router';
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}));
+
+app.use(compression());
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app);
+
+server.listen(6655, () => {
+    console.log('My name is penguin king')
+});
+
+const conn_str = 'mongodb+srv://ishyeen:oyPmJoeSKMpSqEZQ@firstcluster.id8ajii.mongodb.net/tsProject?retryWrites=true&w=majority&appName=firstcluster';
+
+mongoose.Promise = Promise;
+mongoose.connect(conn_str);
+mongoose.connection.on('error', (error: Error) => {console.log(error)});
+
+app.use('/', router())
