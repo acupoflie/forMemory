0fee7cccf7483631b26b0ac8303540a77f30c02f
acupoflie <ishyeen@gmail.com>
2024-06-20 19:26:04 +0400

    fibonacci and factorial

---
+++ jsTutorials/jsAlgorithms/algorithms/notations.js
@@ -0,0 +1,25 @@
+
+/*
+    function fibonacci(n) {
+        const fib = [0,1];
+        for(let i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib;
+    }
+
+    Big O = O(n)
+
+--------------------------------------------------------
+
+    function factorial(n) {
+        let result = 1;
+        for(let i = 2; i <= n; i++) {
+            result = result * i
+        }
+        return result
+    }
+
+    Big O = O(n)
+*/
+
