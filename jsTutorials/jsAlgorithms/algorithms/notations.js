eb312fd6ea2bef9bee559d140a7b58d861642d3d
acupoflie <ishyeen@gmail.com>
2024-06-26 22:04:12 +0400

    recursive complexity

--- jsTutorials/jsAlgorithms/algorithms/notations.js
+++ jsTutorials/jsAlgorithms/algorithms/notations.js
@@ -72,3 +72,17 @@ function isPowerOfTwoBitwise(n) {
 Big O = O(1)
 
 */
+
+
+function recursiveFibonacci(n) {
+    if(n < 2) {
+        return n
+    }
+    return recursiveFibonacci(n-1) + recursiveFibonacci(n-2)
+}
+
+console.log(recursiveFibonacci(0))
+console.log(recursiveFibonacci(1))
+console.log(recursiveFibonacci(6))
+
+    //  Big O = O(2^n) - Recursive  -  Terrible solution
