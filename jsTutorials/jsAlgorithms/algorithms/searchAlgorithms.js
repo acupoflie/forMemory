ecdd7c6c16d69baacb4537d2b8fe3609c95b47ac
acupoflie <ishyeen@gmail.com>
2024-06-30 21:49:41 +0400

    linear search

---
+++ jsTutorials/jsAlgorithms/algorithms/searchAlgorithms.js
@@ -0,0 +1,21 @@
+
+/*
+function linearSearch(arr, target) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === target) {
+            return i
+        }
+    }
+    return -1
+}
+
+Big O = O(n)
+
+--------------------------------------------------------
+
+
+// console.log(linearSearch([-5, 2, 10, 4, 6], 10))
+// console.log(linearSearch([-5, 2, 10, 4, 6], 6))
+// console.log(linearSearch([-5, 2, 10, 4, 6], 20))
+
+*/
