3bdec1b967aef6aba642eb839a0a5c458bd2b024
acupoflie <ishyeen@gmail.com>
2024-07-13 18:07:43 +0400

    binary search

--- jsTutorials/jsAlgorithms/algorithms/searchAlgorithms.js
+++ jsTutorials/jsAlgorithms/algorithms/searchAlgorithms.js
@@ -14,8 +14,28 @@ Big O = O(n)
 --------------------------------------------------------
 
 
-// console.log(linearSearch([-5, 2, 10, 4, 6], 10))
-// console.log(linearSearch([-5, 2, 10, 4, 6], 6))
-// console.log(linearSearch([-5, 2, 10, 4, 6], 20))
-
 */
+
+const arr = [-5, 2, 4, 6, 10];
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length -1;
+
+    while(left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        if(target === arr[middle]) {
+            return middle
+        }
+        if(target < arr[middle]) {
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
+    }
+    return -1
+}
+
+console.log(binarySearch(arr, 10))
+console.log(binarySearch(arr, 6))
+console.log(binarySearch(arr, 20))
