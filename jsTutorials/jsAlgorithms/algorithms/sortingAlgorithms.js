ed35f49d860155c80d8708a1120d935f487a1fcb
acupoflie <ishyeen@gmail.com>
2024-07-14 01:33:05 +0400

    quick sort

--- jsTutorials/jsAlgorithms/algorithms/sortingAlgorithms.js
+++ jsTutorials/jsAlgorithms/algorithms/sortingAlgorithms.js
@@ -43,5 +43,26 @@ Big O = O(n^2)
 
 --------------------------------------------------------
 
-*/
+let arr = [8, 20, -2, 4, -6];
 
+function quickSort(arr) {
+    if(arr.length < 2) return arr
+
+    let pivot = arr[arr.length - 1];
+    let left = []
+    let right = []
+
+    for(let i = 0; i < arr.length - 1; i++) {
+        if(arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+Big O = O(n logn)
+
+*/
