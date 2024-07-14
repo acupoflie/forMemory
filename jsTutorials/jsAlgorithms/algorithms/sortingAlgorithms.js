a2ae2836960f7555b488bd56314c30ca99c0a22f
acupoflie <ishyeen@gmail.com>
2024-07-14 14:18:50 +0400

    merge sort

--- jsTutorials/jsAlgorithms/algorithms/sortingAlgorithms.js
+++ jsTutorials/jsAlgorithms/algorithms/sortingAlgorithms.js
@@ -36,7 +36,7 @@ function insertionSort(arr) {
         }
 
         arr[se+1] = nti;
-    }
+    }   
 }
 
 Big O = O(n^2)
@@ -64,5 +64,35 @@ function quickSort(arr) {
 }
 
 Big O = O(n logn)
-bla bla blank
+
 */
+
+let arr = [8, 20, -2, 4, -6];
+
+function mergeSort(arr) {
+    if(arr.length < 2) {
+        return arr
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid)
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+function merge(left, right) {
+    const sortedArr = [];
+    while(left.length && right.length) {
+        if(left[0] <= right[0]) {
+            sortedArr.push(left.shift())
+        } else {
+            sortedArr.push(right.shift())
+        }
+    }
+    return [...sortedArr, ...left, ...right]
+}
+
+console.log(mergeSort(arr));
+
+// Big O = O(n logn)
