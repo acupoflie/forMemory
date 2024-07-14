36d4e70ad13df29a10593fe4f171e3e5733f01a4
acupoflie <ishyeen@gmail.com>
2024-07-14 16:59:13 +0400

    cartesian product

---
+++ jsTutorials/jsAlgorithms/algorithms/miscProblems.js
@@ -0,0 +1,23 @@
+/*
+
+function cartesianProduct(arr1, arr2) {
+    const result = [];
+
+    for(let i = 0; i < arr1.length; i++) {
+        for(let j = 0; j < arr2.length; j++) {
+            result.push([arr1[i], arr2[j]]);
+        }
+    }
+
+    return result;
+}
+
+? m and n is th arrays
+? Big O = O(mn)
+
+const arr1 = [1, 2];
+const arr2 = [3, 4, 5];
+console.log(cartesianProduct(arr1, arr2));
+
+*/
+
