7e56d1d481dcbdd50121c70cd6facf94860ac721
acupoflie <ishyeen@gmail.com>
2024-07-17 00:41:20 +0400

    tower of hanoi

--- jsTutorials/jsAlgorithms/algorithms/miscProblems.js
+++ jsTutorials/jsAlgorithms/algorithms/miscProblems.js
@@ -33,3 +33,18 @@ function climbingStaircase(n) {
 Big O = O(n)
 
 */
+
+
+function towerOfHanoi(n, fromRod, toRod, usingRod) {
+    if(n === 1) {
+        console.log(`Move disk 1 from ${fromRod} to ${toRod}`);
+        return;
+    }
+    towerOfHanoi(n-1, fromRod, usingRod, toRod);
+    console.log(`Move disk ${n} from ${fromRod} to ${toRod}`)
+    towerOfHanoi(n-1, usingRod, toRod, fromRod);
+    console.log(``)
+}
+
+
+towerOfHanoi(3, 'A', 'C', 'B')
