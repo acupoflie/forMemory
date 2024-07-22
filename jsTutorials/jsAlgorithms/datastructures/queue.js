f8457fc0c74ef5ed41c677f027ecf3828522a014
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-07-22 05:04:06 +0400

    reg and optimised queue

--- jsTutorials/jsAlgorithms/datastructures/queue.js
+++ jsTutorials/jsAlgorithms/datastructures/queue.js
@@ -1,4 +1,6 @@
 
+//! REGULAR QUEUE
+
 // class Queue {
 //     constructor() {
 //         this.items = []
@@ -35,51 +37,37 @@
 
 //! OPTIMISED QUEUE
 
-class Queue {
-    constructor() {
-        this.items = {}
-        this.rear = 0
-        this.front = 0
-    }
-    enqueue(el) {
-        this.items[this.rear] = el;
-        this.rear++
-    }
+// class Queue {
+//     constructor() {
+//         this.items = {}
+//         this.rear = 0
+//         this.front = 0
+//     }
+//     enqueue(el) {
+//         this.items[this.rear] = el;
+//         this.rear++
+//     }
 
-    dequeue() {
-        const item = this.items[this.front]
-        delete this.items[this.front]
-        this.front++
-        return item
-    }
+//     dequeue() {
+//         const item = this.items[this.front]
+//         delete this.items[this.front]
+//         this.front++
+//         return item
+//     }
 
-    isEmpty() {
-        return this.rear - this.front === 0
-    }
+//     isEmpty() {
+//         return this.rear - this.front === 0
+//     }
 
-    peek() {
-        return this.items[this.front]
-    }
+//     peek() {
+//         return this.items[this.front]
+//     }
 
-    size() {
-        return this.rear - this.front
-    }
+//     size() {
+//         return this.rear - this.front
+//     }
 
-    print() {
-        console.log(this.items)
-    }
-}
-
-const que = new Queue()
-console.log(que.isEmpty())
-
-que.enqueue(10)
-que.enqueue(20)
-que.enqueue(30)
-console.log(que.size())
-que.print()
-
-console.log(que.dequeue())
-que.print()
-que.enqueue(10)
-que.print()
+//     print() {
+//         console.log(this.items)
+//     }
+// }
